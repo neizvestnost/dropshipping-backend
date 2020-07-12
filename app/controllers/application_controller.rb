5140b69dc6844b30b2f93e0b9b041b15ddb9707b
Maksym 19 <mherasevych@sunscript.com.ua>
2020-07-12 12:01:48 +0300

    Feat: add graphql, add routes, add user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  private
+
+  def authenticate_user
+    @decoded = Auth::JwtWebToken.decode(request.headers['Auth'])
+  rescue JWT::DecodeError, JWT::VerificationError
+    render json: { errors: 'Incorrect token' }, status: :unauthorized
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: @decoded[:user_id])
+  end
 end
