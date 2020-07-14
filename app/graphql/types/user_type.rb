5284338e2e24259cc0c3d3f404824397683216d8
Maksym 19 <mherasevych@sunscript.com.ua>
2020-07-14 13:59:41 +0300

    Fix: Add some improvments

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,8 +3,5 @@
 module Types
   class UserType < Types::BaseObject
     field :email, String, null: false
-    field :phone_number, String, null: false
-    field :first_name, String, null: false
-    field :last_name, String, null: false
   end
 end
